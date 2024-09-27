fd32974385d4a165d1e566f7137cec0cf0b8faae
Sangwoo <jswoo1995@gmail.com>
2024-09-27 14:36:29 +0900

    not-found 홈으로 돌아가기 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+import style from "./not-found.module.css";
+
 export default function NotFound() {
-    return <div>404: NotFound</div>;
+    return (
+        <div className={style.container}>
+            <h2>올바르지 않은 주소입니다.</h2>
+            <Link href={"/"}>홈으로 돌아가기</Link>
+        </div>
+    );
 }
